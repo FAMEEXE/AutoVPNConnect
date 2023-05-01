d3b61d2c100d9218ad94498111804bed4102b87a
Felix Eckl <felixeckl@outlook.com>
2023-05-01 20:16:59 +0200

    Rename

--- REFame.Wpf.AutoConnector/View/Resources.Designer.cs
+++ REFame.Wpf.AutoConnector/View/Resources.Designer.cs
@@ -38,7 +38,7 @@ namespace Ibf.Wpf.AutoConnector.View {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Ibf.Wpf.AutoConnector.View.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("REFame.Wpf.AutoConnector.View.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
